6a23de0eec3de12b77d465ed9d0181a9cc670958
Ronald Liu <rkkl90@gmail.com>
2018-11-26 10:23:06 -0800

    Updated Auctions and Bids Controller

--- biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -1,13 +1,32 @@
 class Api::V1::AuctionsController < ApplicationController
     def create
+        auction = Auction.new auction_params
+
+        auction.save!
+        render json: auction
     end
 
     def show
+        render json: auction
     end
 
     def index
+        auctions = Auction.order created_at: :desc
+        render json: auctions
     end
 
     def destroy
+        auction.destroy
+        render json: {status: :success}
+    end
+
+    private
+
+    def auction
+        Auction.find params[:id]
+    end
+
+    def auction_params
+        params.require(:auction).permit(:title, :description, :end_date, :price)
     end
 end
--- biddr_api/app/controllers/api/v1/bids_controller.rb
+++ biddr_api/app/controllers/api/v1/bids_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::BidsController < ApplicationController
+    def create
+        bid = Bid.new bid_params
+
+        bid.save!
+        render json: bid
+    end
+
+    private
+    def bid_params
+        params.require(:bid).permit(:price)
+    end
 end
