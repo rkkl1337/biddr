298ae78b2709d99167e2838d1d32f23b8341c023
Ronald Liu <rkkl90@gmail.com>
2018-11-26 09:59:47 -0800

    Added Validations on Models

--- biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::AuctionsController < ApplicationController
+    def create
+    end
+
+    def show
+    end
+
+    def index
+    end
+
+    def destroy
+    end
 end
--- biddr_api/app/models/auction.rb
+++ biddr_api/app/models/auction.rb
@@ -1,2 +1,10 @@
 class Auction < ApplicationRecord
+    belongs_to :user
+    has_many :bids, dependent: :destroy
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :end_dage, presence: true
+    validates :price, presence: true
+
 end
