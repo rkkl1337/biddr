f39b42f08b8d5b69083673ae679606ffc1ea87ec
Ronald Liu <rkkl90@gmail.com>
2018-11-26 17:50:24 -0800

    Implemented Show Auction Page

--- biddr_react/src/components/AuctionDetails.js
+++ biddr_react/src/components/AuctionDetails.js
@@ -6,6 +6,8 @@ function AuctionDetails(props) {
         <p>{props.title}</p>
         <p>{props.details}</p>
         <p>By {props.user.username}</p>
+        <p>Ends on: {props.end_date}</p>
+        <p>Starting price: ${props.price}</p>
         <p>Created at: {props.created_at}</p>
       </div>
     );
